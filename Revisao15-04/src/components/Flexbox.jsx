065ee2783ed2ae93a4a271519b7e0c563d433ed4
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-04-15 09:36:12 -0300

    revisao

---
+++ Revisao15-04/src/components/Flexbox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Flexbox(){
+    return(
+        <div>
+            <div>
+                <p>Item 1</p>
+            </div>
+            <div>
+                <p>Item 2</p>
+            </div>
+            <div>
+                <p>Item 3</p>
+            </div>
+        </div>
+    )
+}

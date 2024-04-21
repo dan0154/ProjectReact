683da19b6f3f252d98aba4014b459eae106a38d7
dan01542 <danielschmitzsc@gmail.com>
2024-04-21 12:28:17 -0300

    quase final

--- revisao-prova-01/src/App.jsx
+++ revisao-prova-01/src/App.jsx
@@ -6,12 +6,19 @@ import "./styles/styles.css"
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Gallery from './components/Gallery'
+import ListItems from './components/ListItems'
 
 function App() {
   return (
     <>
-      <Header />
-      <Gallery />
+      <div className='content'>
+        <Header />
+        <Gallery />
+        <div className='list'>
+          <ListItems item1={"Leclerc, Charles"} item2={"26 y"} />
+          <ListItems item1={"Sainz, Carlos"} item2={"29 y"} />
+        </div>
+      </div>
       <Footer />
     </>
   )

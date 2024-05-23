1cea0f5547284125d793174fbb6276729548593f
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-05-23 09:41:18 -0300

    Inicio

---
+++ Pokedex23-05/src/components/Pokemon.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import "../style/Pokemon.css"
+export default function Pokemon(){
+    const [ id, setId ] = useState(1) //iniciando id com valor 1
+    const [ pokemon, setPokemon ] = useState(null) //iniciando dado pokemon com valor nulo
+
+    const fetchData = async () => {
+        try{
+            const response = await fetch (`https://pokeapi.co/api/v2/pokemon/${id}`);
+            const data = await response.json();
+            setPokemon(data)
+        } catch(error) {
+            console.error("Erro: ", error)
+        }
+    } 
+
+    useEffect(() => {
+        fetchData()
+    }, [id]
+    );
+
+    const nextPokemon = () => {
+        setId(id+1);
+    }
+
+    const previousPokemon = () => {
+        if(id > 1){
+            setId(id-1);
+        }
+        
+    }
+
+    return(
+        
+        <div>
+            
+            {pokemon && (
+                <div className="pokemon">
+                    <p>{pokemon.name}</p>
+                    <img src={pokemon.sprites.front_default}></img>
+                    <div>
+                        <button onClick={previousPokemon}>Previous</button>
+                        <button onClick={nextPokemon}>Next</button>
+                    </div>
+
+                </div>
+            )
+            
+            }
+        </div>
+    )
+}
